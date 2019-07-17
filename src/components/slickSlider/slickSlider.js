53824b6e45a59dcf13f3a4a40d22718209d49648
Evgeny <bespalove.mir@gmail.com>
2019-07-17 16:58:15 +0300

    Добавил главный слайдер

--- src/components/slickSlider/slickSlider.js
+++ src/components/slickSlider/slickSlider.js
@@ -3,11 +3,12 @@ import slick from 'slick-carousel';
 
 const runSlider = () => {
 	
-	$('.j-slider').slick({
-		autoplay: true,
-		autoplaySpeed: 3000,
+	$('.j-slider-main').slick({
+		// autoplay: true,
+		// autoplaySpeed: 3000,
 		speed: 1000,
-		arrows: false,
+		prevArrow: $('.j-main-slide-prev'),
+		nextArrow: $('.j-main-slide-next'),
 		dots: true,
 		dotsClass: 'b-slider__dots'
 	});
--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,16 @@
 // import objectFitImages from 'object-fit-images'; // => Полифил для картинок с object-fit 
 import Header from '../pages/layout/_header.js';
+import MainSlider from '../components/slickSlider/slickSlider.js';
 
 
 /* Подключаем прелоадер */
 const headerItem = document.querySelector('.j-header-discount');
 if(headerItem) {
 	Header();
+}
+
+/* Подключаем слайдер на главном экране */
+const mainSliuderItem = document.querySelector('.j-slider-main');
+if(mainSliuderItem) {
+	MainSlider();
 }
